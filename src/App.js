c4ebf3aecdd6318cc3242e20917fdbddb883aeb1
Isa35160 <teixeirarisabel@gmail.com>
2019-03-05 11:24:54 +0100

    Création composants message et liste message

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
 import FormMessage from './components/FormMessage';
+import ListMessages from "./components/ListMessages";
 
 class App extends Component {
-  render() {
+
+    constructor(props) {
+        super(props);
+        this.state = { messages: []};
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:4000/messages')
+            //récupere uniquement les données au format json
+            .then(response => response.json())
+            .then(data => this.setState({ messages: data}))
+    }
+
+    render() {
     return (
       <div className="App">
         <header className="App-header">
             <FormMessage/>
+            <ListMessages messages={this.state.messages}/>
         </header>
       </div>
     );
--- src/components/ListMessages.js
+++ src/components/ListMessages.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
+import Message from "./Message";
 
 class ListMessages extends Component {
     render() {
+
+        //permet de transformer un tableau de données en composant React
+        const messages =  this.props.messages.map(message => <Message/>)
+
         return (
             <div>
-                
+
             </div>
         );
     }
